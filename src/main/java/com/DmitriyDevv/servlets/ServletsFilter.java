22d68bbd61462ee0c2c856eec159d95e8cd9f387
Dmitriy <planeta977@mail.ru>
2024-06-24 00:15:37 +0300

    feat: added CORS

--- src/main/java/com/DmitriyDevv/servlets/ServletsFilter.java
+++ src/main/java/com/DmitriyDevv/servlets/ServletsFilter.java
@@ -6,16 +6,29 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @WebFilter({"/currencies", "/exchangeRate/*", "/currency/*", "/exchangeRates", "/exchange"})
 public class ServletsFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-        throws IOException, ServletException {
+    public void doFilter(
+            ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
         servletRequest.setCharacterEncoding("UTF-8");
         servletResponse.setContentType("application/json; charset=UTF-8");
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader(
+                "Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        response.setHeader("Access-Control-Max-Age", "3600"); // 1 hour
+
         filterChain.doFilter(servletRequest, servletResponse);
     }
 }
